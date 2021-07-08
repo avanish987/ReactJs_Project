82d0708f2232990fce1ea0273adcd14db60bcd7a
Avanish <avanishyadavya2018@gmail.com>
2021-07-08 11:03:44 +0530

    reactjs project first commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,38 @@
+const Sdata = [
+    {  
+        sname:"DARK",
+        imgsrc:"https://cultartes.com/wp-content/uploads/2020/09/Dark-Season-3-German-series-is-streaming-online-on-Netflix-with-English-subtitles-release-date-27th-June-1.jpg",
+       title:"A netflix Original Series",
+       link:"https://www.netflix.com/in/title/80100172",  
+    },
+
+    {  
+        sname:"EXTRACURRICULAR",
+        imgsrc:"https://m.media-amazon.com/images/M/MV5BNGQxMDcyOTEtZWZkNi00NzgwLWEzNjQtZmFhMGY5ZGRlMTdiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+       title:"A netflix Original Series",
+       link:"https://www.netflix.com/in/title/80990668",  
+    },
+
+    { 
+        sname:"STRANGER THINGS",
+        imgsrc:"https://images-na.ssl-images-amazon.com/images/M/MV5BMjEzMDAxOTUyMV5BMl5BanBnXkFtZTgwNzAxMzYzOTE@._V1_.jpg",
+       title:"A netflix Original Series",
+       link:"https://www.netflix.com/in/title/80057281"  ,
+    },
+
+    {  
+        sname:"DARK" ,
+       imgsrc:"https://cultartes.com/wp-content/uploads/2020/09/Dark-Season-3-German-series-is-streaming-online-on-Netflix-with-English-subtitles-release-date-27th-June-1.jpg",
+       title:"A netflix Original Series",
+       link:"https://www.netflix.com/in/title/80100172" , 
+    },
+
+    { 
+       sname:"DARK" ,
+       imgsrc:"https://cultartes.com/wp-content/uploads/2020/09/Dark-Season-3-German-series-is-streaming-online-on-Netflix-with-English-subtitles-release-date-27th-June-1.jpg",
+       title:"A netflix Original Series",
+       link:"https://www.netflix.com/in/title/80100172" , 
+    },
+];
+
+export default Sdata;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Cards';
+import './index.css';
+import Sdata from './Sdata';
+
+
+ReactDOM.render(
+  <>
+     <h1 className="heading_style">List of top 5 Netflix in 2020</h1>
+    
+       {Sdata.map((val) =>{
+        return (
+          <Card 
+          imgsrc={val.imgsrc}
+          title={val.title}
+          sname={val.sname}
+          link={val.link}  
+        />
+       );
+      })}
+      
+  </>,
+  document.getElementById('root')
+);
